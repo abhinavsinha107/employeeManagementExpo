23eca404b2084d86bb0bb7bdad263ad7b8d94cab
Salony Gupta <abhinavsinha135@gmail.com>
2024-01-29 16:55:36 +0530

    basic setup

---
+++ api/src/models/employee.schema.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    employeeId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    employeeName: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    joiningDate: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    },
+    activeEmployee: {
+        type: Boolean,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const Employee = mongoose.model("employee", employeeSchema);
+
+export default Employee;
---
+++ app/(home)/index.tsx
@@ -0,0 +1,100 @@
+import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather, Entypo, Ionicons } from "@expo/vector-icons";
+
+const index = () => {
+  return (
+    <ScrollView>
+      <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ flex: 1 }}>
+        <View style={{ padding: 12 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Feather name="bar-chart" size={24} color="black" />
+            <Text style={{ fontSize: 16, fontWeight: "600" }}>
+              Employee Management System
+            </Text>
+            <Entypo name="lock" size={24} color="black" />
+          </View>
+
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 20,
+            }}
+          >
+            <Pressable
+              style={{
+                backgroundColor: "#D3CCE3",
+                padding: 12,
+                borderRadius: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="people-sharp" size={24} color="black" />
+              </View>
+              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+                Employee List
+              </Text>
+            </Pressable>
+            <Pressable
+              style={{
+                backgroundColor: "#D3CCE3",
+                padding: 12,
+                borderRadius: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="people-sharp" size={24} color="black" />
+              </View>
+              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+                Mark Attendance
+              </Text>
+            </Pressable>
+          </View>
+
+          <View>
+            <Pressable>
+                <View></View>
+            </Pressable>
+          </View>
+        </View>
+      </LinearGradient>
+    </ScrollView>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
